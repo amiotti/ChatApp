4f0310f9f6f4078f06abbbcfe3bc8d386e2524f0
Agustin MIOTTI <amiotti@secco.local>
2022-03-31 23:41:50 -0300

    .env on gitignore

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+//enviroment configurations
+
 const devConfig = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}`;
 const proConfig = process.env.DB_URL;
 
