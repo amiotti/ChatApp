c519e3c5f3855a7e78aa092760fa19ac76e2bc33
Agustin MIOTTI <amiotti@secco.local>
2022-03-31 23:34:15 -0300

    ready to deploy

--- client/src/components/Socket.js
+++ client/src/components/Socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-let socket = io("http://localhost:3000");
+let socket = io("/" /*"http://localhost:3000"*/); // just "/" becouse proxy is setted on package.json
 
 export default socket;
